789415548cd143fb7c36e727541259240351b35b
Wazir <ahsankamal25@gmail.com>
2024-10-05 15:07:49 +0500

    first page of Aora App has been completed

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -24,7 +24,7 @@ const TabsLayout = () => {
     <>
       <Tabs
         screenOptions={{
-          tabBarShowLabel: true,
+          tabBarShowLabel: false,
           tabBarActiveTintColor : '#FFA001',
           tabBarInactiveTintColor : '#CDCDE0',
           tabBarStyle : {
--- app/index.jsx
+++ app/index.jsx
@@ -15,21 +15,44 @@
 
 
 
-import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-// import { Text, View } from 'react-native';
+import { Image, ScrollView, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../constants';
 
 export default function App() {
   return (
 
-   <View className='flex-1 items-center justify-center bg-pink-100'>
-      <Text className="text-3xl font-pblack">Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{color : 'blue'}}> Go to Home
-      </Link>
-    </View>
+   <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <View className={'w-full items-center justify-center px-4'}>
+        {/* <View className={'w-full items-center justify-center h-full px-4'}> */}
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[84px]'
+            resizeMode = 'contain'
+          />
+          <Image
+            source={images.cards}
+            className='max-w-[380px] w-full h-[300px]'
+            resizeMode = 'contain'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-white text-3xl font-bold text-center'>
+              Discover Endless Possibilities With {' '}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+            <Image
+                source={images.path}
+                className='w-[136px] h-[15px] absolute -bottom-2 -right-8'
+                resizeMode='contain'
+              />
+          </View>
+
+        </View>
+      </ScrollView>
+   </SafeAreaView>    
   );
 }
 
