0c77913fe726a2133c2ab4611f1275b6d20f1f19
Wazir <ahsankamal25@gmail.com>
2024-10-24 23:30:35 +0500

    signin page basic layout complete top bar process for all routes route will be done later on

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,9 +1,11 @@
-import { View, Text, ScrollView, Image } from 'react-native'
+import { View, Text, ScrollView, Image} from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { images } from '../../constants'
 import FormField from '../../components/FormField'
+import CustomButton from '../../components/CustomButton';
+import { Link } from 'expo-router';
 
 const SignIn = () => {
 
@@ -11,6 +13,10 @@ const SignIn = () => {
     email : '',
     password : ''
   })
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const submit = () => {
+
+    }
 
   return (
       <SafeAreaView className="bg-primary h-full">
@@ -30,12 +36,22 @@ const SignIn = () => {
             otherStyles = "mt-7"
             keyboardType="email-address"
           />
-          {/* <FormField
+          <FormField
             title="Password"
             Value={form.password}
             handleChangeText={(e) => setForm({...form, password: e})}
             otherStyles = "mt-7"
-          /> */}
+          />
+          <CustomButton
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-gray-100 text-lg font-pregular">Don't have an account?</Text>
+            <Link href="/sign-up" className="text-lg text-secondary font-psemibold">Sign Up</Link>
+          </View>
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/FormField.jsx
+++ components/FormField.jsx
@@ -8,15 +8,15 @@ const FormField = ({title, value, handleChangeText,
 
     const [showPassword, setShowPassword] = useState(false)
   return (
-    <View className={`space-y-2 bg-pink-300 ${otherStyles}`}>
+    <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-gray-100 font-pmedium text-base">{title}</Text>
 
-      <View className="bg-black-100 w-full h-16 px-4 
+      <View className="bg-black-100 w-full h-16 px-4
       border-2 border-black-200 rounded-2xl
-       focus:border-secondary items-center">
+       focus:border-secondary items-center flex-row">
 
       <TextInput
-        className="flex-1 text-white bg-cyan-300 font-psemibold text-base"
+        className="flex-1 text-white font-psemibold text-base"
         value={value}
         placeholder={placeholder}
         // placeholderTextColor="#7b7b8b"
